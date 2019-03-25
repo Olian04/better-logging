8131e5b13538b431c9700bf3e22f89d57f0c1025
Oliver <rift95@live.se>
2019-03-25 11:43:11 +0100

    added example

---
+++ examples/express-middleware.js
@@ -0,0 +1,20 @@
+const betterLogging = require('../src/better-logging');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Init betterLogging
+betterLogging(console, {
+    stampColor: Color => Color.Light_Blue
+});
+
+// Setup middleware
+app.use(betterLogging.expressMiddleware(console));
+
+app.get('/', (req, res) => {
+    res.status(200).send('OK');
+});
+
+app.listen(PORT, () => 
+    console.info(`Listening on port ${PORT}`)
+);
