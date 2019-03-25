c744741d7b35575f4e7fc903fa9f0303ab0b5465
Oliver <rift95@live.se>
2019-03-25 11:42:52 +0100

    added middleware implementation

--- src/better-logging.js
+++ src/better-logging.js
@@ -18,6 +18,7 @@ const Color = {
   Light_Purple: '\033[1;35m',
   Yellow: '\033[1;33m',
   White: '\033[1;37m',
+  STAMP_COLOR: '',
   RESET: '\033[0m'
 }
 
@@ -78,6 +79,7 @@ const betterLogging_internal = ({ log, info, warn, error, debug }) => {
     const typeColors = config.typeColors(Color); // type of options.typeColors is a functions that takes a Color object and returns the same structure as defaultsOptions.typeColors
     const stampColor = config.stampColor(Color);
     hostObj.color = Color;
+    hostObj.color.STAMP_COLOR = stampColor; // used in middleware
 
     let loglevel = 3;
     Object.defineProperty(hostObj, 'loglevel', { 
@@ -126,3 +128,42 @@ const betterLogging = betterLogging_internal(console);
 module.exports = betterLogging;
 module.exports.default = betterLogging;
 module.exports.CustomInstance = betterLogging_internal;
+
+module.exports.expressMiddleware = (hostObj, config = {}) => (req, res, next) => {
+  if (hostObj === undefined || hostObj.line === undefined) {
+    throw new Error('betterLogging#expressMiddleware requires an object decorated by betterLogging as its first argument.');
+  }
+  const useValueOrFallback = (obj, key, fallback) => obj ? obj[key] || fallback : fallback;
+  const ip = {
+    show: useValueOrFallback(config.ip, 'show', true),
+    color: useValueOrFallback(config.ip, 'color', hostObj.color.STAMP_COLOR),
+    value: req.ip || ''
+  }
+  const path = {
+    show: useValueOrFallback(config.path, 'show', true),
+    color: useValueOrFallback(config.path, 'color', hostObj.color.RESET),
+    value: req.path || ''
+  }
+  const body = {
+    show: useValueOrFallback(config.body, 'show', false),
+    color: useValueOrFallback(config.body, 'color', hostObj.color.RESET),
+    value:  req.body || ''
+  }
+  const header = {
+    show: useValueOrFallback(config.header, 'show', false),
+    color: useValueOrFallback(config.header, 'color', hostObj.color.RESET),
+    value: req.headers || ''
+  }
+  hostObj.info(
+    [ip, path, body, header]
+      .map(obj => obj.show 
+        ? `${obj.color}${
+          typeof obj.value === 'object'
+            ? JSON.stringify(obj.value)
+            : obj.value 
+        }${hostObj.color.RESET}` 
+        : '')
+      .join(' ')
+  );
+  next();
+};
