32ce8f932b367c3119e20a3b80226227a3e30374
Oliver <rift95@live.se>
2020-04-05 20:21:37 +0200

    fixed type issue

--- src/lib/util/deepPartial.ts
+++ src/lib/util/deepPartial.ts
@@ -2,6 +2,7 @@
  * This really should be its own library.
  * I always end up writing this piece of code in my projects....
  */
+type Func = (...args: any[]) => any;
 export type DeepPartial<T extends object> = {
-  [P in keyof T]?: T[P] extends object ? (T[P] extends Function ? T[P] : DeepPartial<T[P]>) : T[P];
+  [P in keyof T]?: T[P] extends object ? (T[P] extends Func ? T[P] : DeepPartial<T[P]>) : T[P];
 };
