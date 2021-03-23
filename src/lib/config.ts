f3f00dc634f3bc270b28251a780db34cc4780047
Oliver <rift95@live.se>
2021-03-23 14:59:32 +0100

    added tests for undefined config props

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,8 @@
-import { DeepPartial, Record, useValueOrFallback } from '@olian/typescript-helpers';
+import {
+  DeepPartial,
+  Record,
+  useValueOrFallback,
+} from '@olian/typescript-helpers';
 import { FormattingContext } from './interfaces/formatting.context';
 import { MessageConstructionStrategy } from './enums/messageConstructionStrategy';
 import { Theme } from './interfaces/theme';
@@ -23,7 +27,7 @@ export type PartialConfig = DeepPartial<Config>;
 
 export const DefaultConfig = new Config({
   messageConstructionStrategy: MessageConstructionStrategy.ALL,
-  format: ctx => `${ctx.time24} ${ctx.type} ${ctx.msg}`,
+  format: (ctx) => `${ctx.time24} ${ctx.type} ${ctx.msg}`,
   saveToFile: null,
   color: defaultTheme,
   logLevels: {
@@ -32,30 +36,110 @@ export const DefaultConfig = new Config({
     info: 2,
     line: 1,
     warn: 1,
-    error: 0
+    error: 0,
   },
 });
 
-export const resolveConfig = (config: PartialConfig) => new Config({
-  messageConstructionStrategy: useValueOrFallback(config, 'messageConstructionStrategy', DefaultConfig.messageConstructionStrategy),
-  format: useValueOrFallback(config, 'format', DefaultConfig.format),
-  saveToFile: useValueOrFallback(config, 'saveToFile', null),
-  color: !config.color ?  DefaultConfig.color : {
-    base: useValueOrFallback(config.color, 'base', DefaultConfig.color.base),
-    type: !config.color.type ? DefaultConfig.color.type : {
-      debug: useValueOrFallback(config.color.type, 'debug', DefaultConfig.color.type.debug),
-      error: useValueOrFallback(config.color.type, 'error', DefaultConfig.color.type.error),
-      info: useValueOrFallback(config.color.type, 'info', DefaultConfig.color.type.info),
-      log: useValueOrFallback(config.color.type, 'log', DefaultConfig.color.type.log),
-      warn: useValueOrFallback(config.color.type, 'warn', DefaultConfig.color.type.warn),
-    }
-  },
-  logLevels: !config.logLevels ? DefaultConfig.logLevels : {
-    debug: useValueOrFallback(config.logLevels, 'debug', DefaultConfig.logLevels.debug),
-    error: useValueOrFallback(config.logLevels, 'error', DefaultConfig.logLevels.error),
-    info: useValueOrFallback(config.logLevels, 'info', DefaultConfig.logLevels.info),
-    log: useValueOrFallback(config.logLevels, 'log', DefaultConfig.logLevels.log),
-    warn: useValueOrFallback(config.logLevels, 'warn', DefaultConfig.logLevels.warn),
-    line: useValueOrFallback(config.logLevels, 'line', DefaultConfig.logLevels.line),
-  }
-});
+// Treats undefined & null in the same way a missing key
+const treatAsFalsy = [null, undefined];
+
+export const resolveConfig = (config: PartialConfig) =>
+  new Config({
+    messageConstructionStrategy: useValueOrFallback(
+      config,
+      'messageConstructionStrategy',
+      DefaultConfig.messageConstructionStrategy,
+      treatAsFalsy
+    ),
+    format: useValueOrFallback(
+      config,
+      'format',
+      DefaultConfig.format,
+      treatAsFalsy
+    ),
+    saveToFile: useValueOrFallback(config, 'saveToFile', null, treatAsFalsy),
+    color: !config.color
+      ? DefaultConfig.color
+      : {
+          base: useValueOrFallback(
+            config.color,
+            'base',
+            DefaultConfig.color.base,
+            treatAsFalsy
+          ),
+          type: !config.color.type
+            ? DefaultConfig.color.type
+            : {
+                debug: useValueOrFallback(
+                  config.color.type,
+                  'debug',
+                  DefaultConfig.color.type.debug,
+                  treatAsFalsy
+                ),
+                error: useValueOrFallback(
+                  config.color.type,
+                  'error',
+                  DefaultConfig.color.type.error,
+                  treatAsFalsy
+                ),
+                info: useValueOrFallback(
+                  config.color.type,
+                  'info',
+                  DefaultConfig.color.type.info,
+                  treatAsFalsy
+                ),
+                log: useValueOrFallback(
+                  config.color.type,
+                  'log',
+                  DefaultConfig.color.type.log,
+                  treatAsFalsy
+                ),
+                warn: useValueOrFallback(
+                  config.color.type,
+                  'warn',
+                  DefaultConfig.color.type.warn,
+                  treatAsFalsy
+                ),
+              },
+        },
+    logLevels: !config.logLevels
+      ? DefaultConfig.logLevels
+      : {
+          debug: useValueOrFallback(
+            config.logLevels,
+            'debug',
+            DefaultConfig.logLevels.debug,
+            treatAsFalsy
+          ),
+          error: useValueOrFallback(
+            config.logLevels,
+            'error',
+            DefaultConfig.logLevels.error,
+            treatAsFalsy
+          ),
+          info: useValueOrFallback(
+            config.logLevels,
+            'info',
+            DefaultConfig.logLevels.info,
+            treatAsFalsy
+          ),
+          log: useValueOrFallback(
+            config.logLevels,
+            'log',
+            DefaultConfig.logLevels.log,
+            treatAsFalsy
+          ),
+          warn: useValueOrFallback(
+            config.logLevels,
+            'warn',
+            DefaultConfig.logLevels.warn,
+            treatAsFalsy
+          ),
+          line: useValueOrFallback(
+            config.logLevels,
+            'line',
+            DefaultConfig.logLevels.line,
+            treatAsFalsy
+          ),
+        },
+  });
